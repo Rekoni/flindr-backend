fa50814a97b2b80621dcf4b65c2a8a37c5c90a68
Morako <morako@morako.com>
2018-11-10 17:30:27 +0000

    Retrieve flights on start

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var retrieveFlights = require('./lib/retrieve_flights');
+
+//var retrieveTimer = setInterval(retrieveFlights, 10000);
+retrieveFlights();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
